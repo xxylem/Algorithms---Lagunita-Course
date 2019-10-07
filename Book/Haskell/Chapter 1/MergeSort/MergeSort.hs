17509488b9887387695236b8b1ecdc6eb4b06419
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-07 14:20:11 +0100

    Implement MergeSort in Haskell. Move Java classes to new folder.

---
+++ Book/Haskell/Chapter 1/MergeSort/MergeSort.hs
@@ -0,0 +1,28 @@
+module MergeSort where
+
+-- Input: array A of n distinct integers. (We make this more general.)
+-- Output: array with the same integers, sorted from
+-- smallest to largest.
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort [] = []
+mergeSort (x:[]) = [x]
+mergeSort xs = 
+    merge as' bs'
+        where (as, bs) = splitAt ((length xs) `div` 2) xs
+              as' = mergeSort as
+              bs' = mergeSort bs
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) = 
+    if x <= y then x : (merge xs        (y:ys))
+              else y : (merge (x:xs)    ys)
+
+checkListIsOrdered :: (Ord a) => [a] -> Bool
+checkListIsOrdered [] = True
+checkListIsOrdered (x:[]) = True
+checkListIsOrdered (x1:x2:xs) = (x1 <= x2) && (checkListIsOrdered (x2:xs))
+
+
+
