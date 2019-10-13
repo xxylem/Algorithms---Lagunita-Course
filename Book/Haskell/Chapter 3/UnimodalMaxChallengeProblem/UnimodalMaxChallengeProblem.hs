253a5f672bccf2f75a0afc331a13b2ca3b992e51
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-13 09:37:02 +0100

    Add test suite to UnimodalMaxChallengeProblem

--- Book/Haskell/Chapter 3/UnimodalMaxChallengeProblem/UnimodalMaxChallengeProblem.hs
+++ Book/Haskell/Chapter 3/UnimodalMaxChallengeProblem/UnimodalMaxChallengeProblem.hs
@@ -16,28 +16,4 @@ unimodalMax uniArr =
                                 if uniArr ! midPoint1 < uniArr ! midPoint2
                                 then go (midPoint2, iu)
                                 else go (il, midPoint1)
-
--- Example unimodal arrays
-uArr1 :: Array Integer Integer                                
-uArr1 = listArray (1,1) [10]
-uArr2 :: Array Integer Integer                                
-uArr2 = listArray (1,2) [9, 10]
-uArr3 :: Array Integer Integer                                
-uArr3 = listArray (1,2) [10, 9]
-uArr4 :: Array Integer Integer                                
-uArr4 = listArray (1,3) [8, 9, 10]
-uArr5 :: Array Integer Integer                                
-uArr5 = listArray (1,3) [8, 10, 9]
-uArr6 :: Array Integer Integer                                
-uArr6 = listArray (1,3) [9, 10, 8]
-uArr7 :: Array Integer Integer                                
-uArr7 = listArray (1,3) [10, 9, 8]
-uArr8 :: Array Integer Integer                                
-uArr8 = listArray (1,10) [1..10]
-uArr9 :: Array Integer Integer                                
-uArr9 = listArray (1,10) (reverse [1..10])
-uArr10 :: Array Integer Integer                                
-uArr10 = listArray (1,10) [1, 3, 4, 5, 19, 17, 15, 14, 12, 2]
-uArr11 :: Array Integer Integer                                
-uArr11 = listArray (1,10) [1, 17, 23, 22, 21, 18, 15, 13, 9, 5]
-
+                                
---
+++ Book/Haskell/Chapter 3/UnimodalMaxChallengeProblem/UnimodalMaxChallengeProblemTest.hs
@@ -0,0 +1,49 @@
+module UnimodalMaxChallengeProblemTest where
+
+import qualified Data.Array as A
+import Test.Hspec
+import Control.Exception (evaluate)
+
+import UnimodalMaxChallengeProblem (unimodalMax)
+
+
+main :: IO ()
+main = hspec $ do
+    describe "Test suite for finding the max element in a unimodal array." $ do
+        it "throws exception if used with empty array" $ do
+            evaluate (unimodalMax ua0Elem) `shouldThrow` anyException
+        it "the only element in a one element array is the max" $ do
+            unimodalMax ua1Elem `shouldBe` 10
+        it "test two element arrays" $ do
+            unimodalMax ua2Elem1 `shouldBe` 10
+            unimodalMax ua2Elem2 `shouldBe` 10
+        it "test three element arrays" $ do
+            unimodalMax ua3Elem1 `shouldBe` 10
+            unimodalMax ua3Elem2 `shouldBe` 10
+            unimodalMax ua3Elem3 `shouldBe` 10
+            unimodalMax ua3Elem4 `shouldBe` 10
+        it "test longer sorted arrays (one direction)" $ do
+            unimodalMax uaLongerSorted `shouldBe` 10
+            unimodalMax uaLongerReverseSorted `shouldBe` 10
+        it "test longer fully unimodal arrays" $ do
+            unimodalMax uaLongerMixed1 `shouldBe` 19
+            unimodalMax uaLongerMixed2 `shouldBe` 23
+
+ 
+-- Example unimodal arrays
+ua0Elem, ua1Elem, ua2Elem1, ua2Elem2, ua3Elem1,
+    ua3Elem2, ua3Elem3, ua3Elem4,
+    uaLongerSorted, uaLongerReverseSorted,
+    uaLongerMixed1, uaLongerMixed2 :: A.Array Integer Integer
+ua0Elem = A.listArray (1, 0) []                             
+ua1Elem = A.listArray (1,1) [10]                             
+ua2Elem1 = A.listArray (1,2) [9, 10]                             
+ua2Elem2 = A.listArray (1,2) [10, 9]                              
+ua3Elem1 = A.listArray (1,3) [8, 9, 10]
+ua3Elem2 = A.listArray (1,3) [8, 10, 9]
+ua3Elem3 = A.listArray (1,3) [9, 10, 8]
+ua3Elem4 = A.listArray (1,3) [10, 9, 8]
+uaLongerSorted = A.listArray (1,10) [1..10]
+uaLongerReverseSorted = A.listArray (1,10) (reverse [1..10])
+uaLongerMixed1 = A.listArray (1,10) [1, 3, 4, 5, 19, 17, 15, 14, 12, 2]                   
+uaLongerMixed2 = A.listArray (1,10) [1, 17, 23, 22, 21, 18, 15, 13, 9, 5]
