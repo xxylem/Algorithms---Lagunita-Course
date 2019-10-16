7de64d65e81545e62ee8ac8b03948e4e8ef8b9ed
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-16 20:05:01 +0100

    Add test suite for ArrayElementEqIndex

--- Java/src/ListsAndArrays/Find/ArrayElementEqIndex.java
+++ Java/src/ListsAndArrays/Find/ArrayElementEqIndex.java
@@ -2,35 +2,6 @@ package ListsAndArrays.Find;
 
 public class ArrayElementEqIndex {
 
-    public static void main(String[] args) {
-
-
-        // Examples of sorted arrays.
-        int[] arr1 = {0};
-        System.out.println(arrayElementEqIndex(arr1));
-        int[] arr2 = {1};
-        System.out.println(arrayElementEqIndex(arr2));
-        int[] arr3 = {1, 2};
-        System.out.println(arrayElementEqIndex(arr3));
-        int[] arr4 = {0, 2};
-        System.out.println(arrayElementEqIndex(arr4));
-        int[] arr5 = {-1, 1};
-        System.out.println(arrayElementEqIndex(arr5));
-        int[] arr6 = {-1, 0, 1};
-        System.out.println(arrayElementEqIndex(arr6));
-        int[] arr7 = {-1, 0, 2};
-        System.out.println(arrayElementEqIndex(arr7));
-        int[] arr8 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(arrayElementEqIndex(arr8));
-        int[] arr9 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println(arrayElementEqIndex(arr9));
-        int[] arr10 = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1};
-        System.out.println(arrayElementEqIndex(arr10));
-        int[] arr11 = {-10, -9, -8, -7, -6, -5, -4, -3, -2, 9};
-        System.out.println(arrayElementEqIndex(arr11));
-
-    }
-
     /** Given a sorted array (smallest to largest) of n distinct integers
         (positive, negative, or zero), return True if there exists an
         index i such that the element at that index is equal to the index.
@@ -38,7 +9,7 @@ public class ArrayElementEqIndex {
         INPUT: A sorted array of n distinct integers, arr.
         OUTPUT: True if there is i s.t. arr ! i == i. False otherwise.
         ASSUMES: 0-indexing in the array. */
-    private static Boolean arrayElementEqIndex (int[] arr) {
+    static Boolean arrayElementEqIndex(int[] arr) {
 
         int n = arr.length;
         int windowStart = 0;
---
+++ Java/src/ListsAndArrays/Find/ArrayElementEqIndexTest.java
@@ -0,0 +1,61 @@
+package ListsAndArrays.Find;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static ListsAndArrays.Find.ArrayElementEqIndex.arrayElementEqIndex;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayElementEqIndexTest {
+
+    int[] empty, oneElemT, oneElemF, twoElemPosF, twoElem0T, twoElemNegT, threeElemF, threeElemT, longerF, longerT,
+            negLongerF, negLongerT;
+
+    @BeforeEach
+    void setup() {
+        empty = new int[] {};
+        oneElemT = new int[] {0};
+        oneElemF = new int[] {1};
+        twoElemPosF = new int[] {1, 2};
+        twoElem0T = new int[] {0, 2};
+        twoElemNegT = new int[] {-1, 1};
+        threeElemF = new int[] {-1, 0, 1};
+        threeElemT = new int[] {-1, 0, 2};
+        longerF = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        longerT = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        negLongerF = new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1};
+        negLongerT = new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, 9};
+    }
+
+    @Test
+    void oneElementArrays() {
+        assertTrue(arrayElementEqIndex(oneElemT));
+        assertFalse(arrayElementEqIndex(oneElemF));
+    }
+
+    @Test
+    void twoElementArrays() {
+        assertFalse(arrayElementEqIndex(twoElemPosF));
+        assertTrue(arrayElementEqIndex(twoElem0T));
+        assertTrue(arrayElementEqIndex(twoElemNegT));
+    }
+
+    @Test
+    void threeElementArrays() {
+        assertFalse(arrayElementEqIndex(threeElemF));
+        assertTrue(arrayElementEqIndex(threeElemT));
+    }
+
+    @Test
+    void longerArrays() {
+        assertFalse(arrayElementEqIndex(longerF));
+        assertTrue(arrayElementEqIndex(longerT));
+    }
+
+    @Test
+    void arraysOfNegativeElements() {
+        assertFalse(arrayElementEqIndex(negLongerF));
+        assertTrue(arrayElementEqIndex(negLongerT));
+    }
+
+}
